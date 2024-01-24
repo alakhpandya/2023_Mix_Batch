2340d76e23860d1d5a17e06b06240bffcae13c5a
alakhpandya <alakhpandya@gmail.com>
2024-01-24 11:09:14 +0530

    jan24

---
+++ css/project1_teachable/js/main.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut'
+    // dots:false
+})
