d49ae5595d5653653d65ed93579a74d439dc3e00
alakhpandya <alakhpandya@gmail.com>
2024-01-30 10:04:42 +0530

    jan30

--- css/project1_teachable/js/main.js
+++ css/project1_teachable/js/main.js
@@ -2,11 +2,12 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
     items:1,
     autoplay: true,
     autoplayTimeout: 3000,
     autoplayHoverPause:true,
     animateIn: 'fadeIn',
     animateOut: 'fadeOut'
-    // dots:false
+    // dots:false 
 })
